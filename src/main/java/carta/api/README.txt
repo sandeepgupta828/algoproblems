049b7c63c095db4d0e20c5c1d9039eb1135305d1
Sandeep <sandeepgupta828@gmail.com>
2019-12-18 14:02:34 -0800

    Update

---
+++ src/main/java/carta/api/README.txt
@@ -0,0 +1,22 @@
+How to run the program?
+
+After unzipping.
+
+(1)
+Make sure you have gradle installed
+https://gradle.org/install/
+
+(2)
+Make sure you have java installed
+
+(3)
+Go in the directory and build
+./gradlew build
+
+(4)
+Run with arguments:
+java -jar build/libs/algoproblems-1.0-SNAPSHOT.jar /Users/new/IdeaProjects/algoproblems/sample.csv 2019-01-01
+
+
+
+
--- src/main/java/carta/api/Solution.java
+++ src/main/java/carta/api/Solution.java
@@ -16,49 +16,13 @@ import java.util.Date;
 import java.util.List;
 
 /*
-Requirements
-The program you write should take in a path to a CSV file as an argument, and support an optional date parameter, in YYYY-MM-DD format.
-If a date argument is provided, the summary cap table should be computed using investments made on or before the specified date.
-If no date is given, calculate the cap table as of today.
-
-The program you write should take command line arguments.
-
-You may use any OOP programming language, libraries, or frameworks that you like, provided it is easy for us to build and run your code.
-
-INPUT:
-#INVESTMENT DATE, SHARES PURCHASED, CASH PAID, INVESTOR
-2016-04-03,1000,10000.00,Sandy Lerner
-2017-11-14,1000,12000.00,Don Valentine
-2018-01-20,2000,40000.00,Don Valentine
-2018-03-20,2000,40000.00,Ann Miura-Ko
-2019-01-02,2000,50000.00,Sandy Lerner
-2019-01-02,1500,13500.00,Fred Wilson
-
-OUTPUT:
-{
-  "date": "02/01/2019", 	// mm/dd/yyyy format date the captable was requested
-				// (if no filter is implemented, then today)
-  "cash_raised": 165500.00,	// decimal representing total cash paid by
- 					// investors up to date
-  "total_number_of_shares": 9500,	// integer representing the total number
- 						// of shares bought by investors
-  "ownership": [ 	// list of investors and their aggregated investments
-      {
-        investor: "Don Valentine", // investor name
-        shares: 3000, // total amount of shares owned by that given investor
-      	  cash_paid: 52000.00, // total investment made by this investor
-        ownership: 31.58 // decimal representing total investor ownership
-				 // (31.58 means 31.58%)
-      },
-      ...
-  ],
-}
+Solution for processing cap table data
  */
 public class Solution {
     private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new java.text.SimpleDateFormat("yyyy-MM-dd");
 
     public static void main(String[] args) throws IOException, ParseException {
-        System.out.println(processCapTableData("/Users/new/IdeaProjects/algoproblems/src/main/java/carta/api/sampledata.csv", ""));
+        System.out.println(processCapTableData(args.length > 0 ? args[0] : "/Users/new/IdeaProjects/algoproblems/sample.csv", args.length > 1 ? args[1] : ""));
     }
     public static String processCapTableData(String srcFilePath, String dateStr) throws IOException, ParseException {
         Date date = new Date(); // initialize date to current date
@@ -71,7 +35,7 @@ public class Solution {
         String header = bufferedReader.readLine();
         String[] headerElements = header.split(",");
         List<ShareData> shareData = new ArrayList<>();
-        int lineCount = 1;
+        int lineCount = 2; //starts at 1 (which we ignore as header elements)
         Long totalShares = 0L;
         Double cashRaised = 0.00;
         while (true) {
@@ -79,7 +43,9 @@ public class Solution {
             if (line != null) {
                 String[] lineElements = line.split(",");
                 if (lineElements.length  < 4) {
-                    throw new RuntimeException("Invalid input data in file: check line #"+lineCount);
+                    System.out.println("Ignoring invalid data on line #"+lineCount);
+                    lineCount++;
+                    continue;
                 }
                 Date investmentDate = SIMPLE_DATE_FORMAT.parse(lineElements[0]);
                 if (investmentDate.after(date)) {
