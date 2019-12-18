2958334e60be8acd3ab1e9803a3f65b86d519c72
Sandeep <sandeepgupta828@gmail.com>
2019-12-18 13:31:33 -0800

    Remove everything else

--- src/main/java/bingo/WinningPattern.java
+++
@@ -1,9 +0,0 @@
-package bingo;
-
-/**
- * Abstract class for winning patterns
- */
-public abstract class WinningPattern {
-    abstract public String name();
-    abstract public boolean matches(Ticket ticket);
-}
